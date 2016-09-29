80523bd6cc460aa8075d91b17fb6ee8a6644dd8f
thetwentyseven <adrian.vcch@gmail.com>
2016-09-29 15:05:42 +0000

    Chapter 4.1 Request. Exercise 1 (if/get else/post)

--- hello.py
+++ hello.py
@@ -1,30 +1,17 @@
-from flask import Flask, redirect, url_for, abort
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def root():
- return "The default, 'root' route"
+ return "The default, 'root' route" 
 
-@app.route("/hello/")
-def hello():
- return "Hello Napier!!! :D"
-
-@app.errorhandler(404)
-def page_not_found(error):
-  return "Could not find the page you requested.", 404
-
-# This set a new url where will be an img
-@app.route('/static/img')
-def static_img():
-  start = '<img src="'
-  url = url_for('static', filename='vmask.jpg')
-  end = '">'
-  return start+url+end, 200
-
-@app.route("/goodbye/")
-def goodbye():
- return "Goodbye cruel world :("
-
+# Chapter 4 exercise, request
+@app.route("/account/", methods=['GET', 'POST'])
+def account():
+  if request.method == 'POST':
+    return "POST'ed to /account root \n"
+  else:
+    return "GET /account root"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
