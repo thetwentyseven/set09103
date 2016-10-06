ddbe0bb00088f716fca89d1d2248ef69b364da01
thetwentyseven <adrian.vcch@gmail.com>
2016-10-06 15:05:39 +0000

    5.0.3 Templates & Collections, using html templates to render a loop in this case for a list

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# Chapter 5.0.2, using templates with conditional arguments
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-  return render_template('hello.html', name=name)
+# Chapter 5.0.3, using particular templates for particular moments
+@app.route('/users/')
+def users():
+  names = ['Simon', 'Thomas', 'Lee', 'Jamie', 'Sylvester']
+  return render_template('loops.html', name=names)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
