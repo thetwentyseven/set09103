f4c5e1c814c15d9988f127cad8645d02e7bc6c70
thetwentyseven <adrian.vcch@gmail.com>
2016-10-06 13:35:49 +0000

    Chapter 4.1.4, using urls and parameters

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-# Chapter 4.1.3, using other variable types (int, and floats)
-@app.route("/add/<int:first>/<int:second>")
-def add(first, second):
- return str(first+second)
+# Chapter 4.1.4, url and parameters
+@app.route("/hello/")
+def hello():
+  name = request.args.get('name','')
+  if name == '':
+    return "No parameters supplied"
+  else:
+    return "Hello %s" % name
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
