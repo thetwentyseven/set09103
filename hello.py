39abef8bc9189a306a9ed267603d04ac4549d027
thetwentyseven <adrian.vcch@gmail.com>
2016-10-06 14:57:02 +0000

    Templates with conditional arguments

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# Chapter 5.0.1, using templates
-@app.route("/hello/<name>")
+# Chapter 5.0.2, using templates with conditional arguments
+@app.route('/hello/')
+@app.route('/hello/<name>')
 def hello(name=None):
-  user = {'name': name}
-  return render_template('hello.html', user=user)
+  return render_template('hello.html', name=name)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
