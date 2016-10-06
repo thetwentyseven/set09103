4a8b6a961427eeaec65e32bd68fd969eb8637fae
thetwentyseven <adrian.vcch@gmail.com>
2016-10-06 15:30:35 +0000

    Finish chapter 5, inheriting html

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# Chapter 5.0.3, using particular templates for particular moments
-@app.route('/users/')
-def users():
-  names = ['Simon', 'Thomas', 'Lee', 'Jamie', 'Sylvester']
-  return render_template('loops.html', name=names)
+# Chapter 5.0.4, creating different templates for different content
+@app.route('/inherits/')
+def inherits():
+  return render_template('base.html')
+
+@app.route('/inherits/one/')
+def inherits_one():
+  return render_template('inherits1.html')
+
+@app.route('/inherits/two/')
+def inherits_two():
+  return render_template('inherits2.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
