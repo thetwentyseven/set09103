b67beb8a8bf97055184a8b4466aa473204d1aa48
thetwentyseven <adrian.vcch@gmail.com>
2016-10-08 10:18:39 +0000

    Chapter 6.2 Sessions

---
+++ session.py
@@ -0,0 +1,42 @@
+from flask import Flask, session
+
+app = Flask(__name__)
+app.secret_key = 'asdSDAG123121asSssW124^*!sdaS'
+
+@app.route('/')
+def index():
+  return "Root route for the session example"
+
+@app.route('/session/write/<name>')
+def write(name=None):
+  session['name']= name
+  return "Wrote %s into 'name' key of session" % name
+
+@app.route('/session/read/')
+def read():
+  try:
+      if(session['name']):
+        return str(session['name'])
+  except KeyError:
+    pass
+  return "No session variable set for 'name' key"
+
+@app.route('/session/remove/')
+def remove():
+  session.pop('name', None)
+  return "Removed key 'name' from session"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
